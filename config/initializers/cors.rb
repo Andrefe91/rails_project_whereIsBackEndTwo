05d984f4c4a077f09a5a15927f15f37b8f91c9a9
Andres Muñoz <andrefe91@gmail.com>
2024-09-27 18:54:29 -0500

    cors origin update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', 'https://whereisodin.netlify.app/'
+    origins 'http://localhost:5173', 'https://whereisodin.netlify.app'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :options, :head], credentials: true
   end
 end
